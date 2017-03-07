b84821663a5ef446f886273fc63218469bf69ea1
j29rios <j29.rios@gmail.com>
2017-03-07 17:27:31 -0500

    v2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,14 +8,14 @@
 @section('content')
 <!-- Home-->
 <section>
-	<div id="index-banner" class="parallax-container">
+	<div id="index-banner" class="parallax-container parallax-overlay">
 	    <div class="section no-pad-bot">
 	      <div class="container center">
             <!--<img class="center responsive-img whitespace-large img-logo" src="{{URL::asset('img/liderazgo_sostenible.png')}}">-->
 	        <img src="{{URL::asset('img/logo/logo.png')}}" alt="">
-	        <i><h1 class="title-principal wow fadeInDown" data-wow-delay="0.1s">Americas Youth Speak Forum 2017</h1></i>
+	        <h1 class="title-principal wow fadeInDown" data-wow-delay="0.1s">America Youth Speak Forum <br>2017</h1>
             <h5 class="header col s12 light center">
-	        	<p class="wow fadeIn" data-wow-delay="0.4s"><strong>Un evento donde tus ideas serán<br>escuchadas por los tomadores de decisiones.</strong></p>
+	        	<p class="wow fadeIn" data-wow-delay="0.4s"><strong>Un lugar donde la inspiración y la <br>colaboración se convierten en acción</strong></p>
             </h5>            
 	        <div class="row"><div class="section"></div></div>
 	        <!--<div class="row center wow zoomIn" data-wow-delay="0.3s">
@@ -24,19 +24,19 @@
 	      	</div>-->
 	      </div>
 	    </div>
-	    <div class="parallax"><img src="{{URL::asset('img/bg/home.jpg')}}" alt="Unsplashed background img 1"></div>
+	    <div class="parallax"><img src="{{URL::asset('img/bg/home.png')}}" alt="Unsplashed background img 1"></div>
 	</div>
 </section>
 <!-- /Home -->
 
 <!--Explain-->
-<section class="container y-azul-text  section center">
-    <h3 class="y-azul-text whitespace-short sub-title sub-title-x"><i>Un Foro donde la inspiración y la colaboración se convierten en acción</i></h3>
+<section class="container y-azul-text  section center"  id="about">
+    <h3 class="y-azul-text whitespace-short sub-title-x">El foro reunirá a empresas privadas, ONG's, gobierno y sociedad civil para <br>intercambiar ideas, compartir propuestas y generar acciones que <br>contribuyan a los <b>Objetivos del Desarrollo Sostenible</b></h3>
     <section class="row center">
         <div class="col s12 m12 l12 whitespace-large">
             <div class="col s4 m4 l4 center">
             	<i class="material-icons explain-icons">face</i>
-                <h5 class="sub-title">+1000 delegados</h5>
+                <h5 class="sub-title">+800 delegados</h5>
             </div>
             <div class="col s4 m4 l4 center">            	                  
             	<i class="material-icons explain-icons">favorite</i>
@@ -48,28 +48,53 @@
 			</div>
         </div>
     </section>   
+    <section class="row">
+    	<article class="col s12 m6 offset-m3 l6 offset-l3">
+	    	<div class="video-container">
+	        	<iframe width="400" src="//www.youtube.com/embed/IQwGNZlW96A?rel=0" frameborder="0" allowfullscreen></iframe>
+	      	</div>    		
+    	</article>
+    </section>
 </section>
 <!-- /Explain-->
 
 <!-- Call to action-->
 <section>
-	<div class="parallax-overlay">
-	<div id="call_to_action" class="parallax-container">
-	    <div class="section no-pad-bot">
+	<div class="parallax-overlay"  id="info">
+	<div id="call_to_action" class="parallax-container parallax-container-info">
+	    <div class="section no-pad-bot whitespace-large">
+    	<h3 class="whitespace-short sub-title center info-title">Información General</h3>	    	
 	      <div class="container section center">
-	      	<div class="row">
-	      		<div class="col s12 m1 offset-m5 l1 offset-l5">
-	        		<h1 class="call-to-action-title wow fadeInDown" data-wow-delay="0.1s"><i>6</i></h1>
-	      		</div>	      		
-	      		<div class="col s12 m1 l1">
-					<i><h4 class="call-to-action-subtitle wow fadeInDown">ABRIL<br><span class="call-to-action-subtitle">2017</span></h4></i>
-	      		</div>	      		
-	      	</div>
-            <h3 class="header col s12 light center">
-	        	<i><p class="wow fadeIn poli" data-wow-delay="0.4s"><strong>Polideportivo de la PUCP</strong></p></i>
-	        	<i><p class="wow fadeIn" data-wow-delay="0.4s"><strong>Lima, Perú</strong></p></i>
-            </h3>            
-	        <div class="row"><div class="section"></div></div>
+	      	<article class="row">
+	      		<div class="col s12 m4 l4 info-sub info-sub-x">
+	      			<div class="col s12 m12 l12">
+	      				<img class="img-responsive" src="{{URL::asset('img/icons/agenda.png')}}" width="150">	      				
+	      			</div>
+	      			<div class="col s12 m12 l12">
+	      				<h4><span class="light">Jueves</span> <b>06 de Abril</b></h4>	      				
+	      				<h5 class="light">8 am a 8 pm</h5>
+	      			</div>
+	      		</div>
+	      		<div class="col s12 m4 l4 info-sub-y">
+	      			<div class="col s12 m12 l12">
+	      				<img class="img-responsive" src="{{URL::asset('img/icons/location.png')}}" width="150">
+	      			</div>
+	      			<div class="col s12 m12 l12">
+	      				<h5><span class="light">Polideportivo</span> <b>PUCP</b></h5>
+	      				<h5 class="light">Av. Universitaria 1801,<br>San Miguel</h5>
+	      			</div>
+	      		</div>
+	      		<div class="col s12 m4 l4 info-sub-z">
+	      			<div class="col s12 m12 l12">
+	      				<img class="img-responsive" src="{{URL::asset('img/icons/sales.png')}}" width="150">
+	      			</div>
+	      			<div class="col s12 m12 l12">
+	      				<h4><span class="light">General</span> <b>s/. 50</b></h4>
+	      				<h5 class="light">Incluye: Charlas magistrales, <br>talleres, feria laboral, almuerzo<br> y coffee breaks</h5>
+	      				<h5 class="light">Preventa hasta el 15 de Marzo: <br>s/. 45</h5>
+	      			</div>
+	      		</div>
+	      	</article>
 	        <div class="row center wow zoomIn" data-wow-delay="0.3s">
 	        	<div class="section"></div>
 	         	<a href="https://podio.com/webforms/17985408/1208462" target="_blank" id="download-button" class="btn-large waves-effect waves-light modal-trigger y-celeste z-depth-3"><strong>Adquiere tu entrada</strong></a>
@@ -83,65 +108,39 @@
 <!-- /Call to action -->
 
 <!--Explain 2-->
-<section class="container y-azul-text  section">
-    <h3 class="y-azul-text whitespace-short sub-title center">Inspírate, Involúcrate y Actúa frente<br>a la realidad del país</h3>
+<section class="container y-azul-text  section"  id="metodologia">
+    <h3 class="y-azul-text whitespace-short sub-title center"><span class="light">Metodología</span> <b>innovadora</b> <span class="light">para generar resultados</span></h3>
     <section class="row">
-        <article class="col s12 m4 l4">
-			<div class="card sticky-action  column-equal">
-				<div class="card-image waves-effect waves-block waves-light">
-					<img class="activator" src="{{URL::asset('img/facts/cuadro01.jpg')}}">
-			    </div>
-			    <div class="card-content center">
-			      	<span class="card-title activator grey-text text-darken-4">
-			      		<a class="light-blue-text accent-4"><b>Gobierno, Empresas y ONG's en el mismo lugar</b></a>
-			      	</span>
-			      	<p class="card-content-text section">La Hora del Planeta, el movimiento ambiental más grande del mundo que inspira y moviliza a personas de todo el mundo para hacerle frente al cambio climático.</p>
-			      	<div class="section"></div>
-			    </div>
-			</div>
+        <article class="col s12 m4 l4 center">
+        	<h4><b>INSPIRAR</b></h4>
+			<img class="activator" src="{{URL::asset('img/icons/sello1.jpg')}}">
+			<p class="light">Líderes de diferentes sectores <br>comparten perspectivas, <br>dialogan sobre las <br>problemáticas del país e <br>incentivan a los jóvenes a <br>tomar acción.</p>
 		</article>
-		<article class="col s12 m4 l4">
-			<div class="card sticky-action column-equal">
-				<div class="card-image waves-effect waves-block waves-light">
-					<img class="activator" src="{{URL::asset('img/facts/cuadro02.jpg')}}">
-			    </div>
-			    <div class="card-content center">
-			      	<span class="card-title activator grey-text text-darken-4">
-			      		<a class="light-blue-text accent-4"><b>Un evento donde tus ideas importan</b></a>
-			      	</span>
-			      	<p class="card-content-text section">Un espacio para co-crear propuestas y proyectos que contribuyan directamente al desarrollo del país a través de los Objetivos de Desarrollo Sostenible hacia el 2030</p>
-			      	<div class="section"></div>
-			    </div>
-			</div>
+		<article class="col s12 m4 l4 center">
+        	<h4><b>INVOLUCRAR</b></h4>
+			<img class="activator" src="{{URL::asset('img/icons/sello2.jpg')}}">
+			<p class="light">Aliados de la organización <br>dictan talleres con temáticas <br>específicas que motivan el <br>intercambio de ideas, discusión <br>y síntesis de una problemática <br>entre los jóvenes asistentes.</p>
 		</article>
-		<article class="col s12 m4 l4">
-			<div class="card sticky-action column-equal">
-				<div class="card-image waves-effect waves-block waves-light">
-					<img class="activator" src="{{URL::asset('img/facts/cuadro01.jpg')}}">
-			    </div>
-			    <div class="card-content center">
-			      	<span class="card-title activator grey-text text-darken-4">
-			      		<a class="light-blue-text accent-4"><b>¡Ponte en acción!</b></a>
-			      	</span>
-			      	<p class="card-content-text section">Presenta tu propuesta o proyecto a los representantes de las organizaciones. Las mejores propuestas serán premiadas en base a la sostenibilidad, innovación e impacto que generen.</p>
-			      	<div class="section"></div>
-			    </div>
-			</div>
+		<article class="col s12 m4 l4 center">
+        	<h4><b>ACTUAR</b></h4>
+			<img class="activator" src="{{URL::asset('img/icons/sello3.jpg')}}">
+			<p class="light">Los jóvenes son desafiados a <br>crear una iniciativa innovadora <br>o mejorar proyectos ya <br>existentes. Las ideas mejor <br>planteadas serán premiadas al <br>final del día.</p>
 		</article>
+		
         </div>
     </section>   
 </section>
 <!-- /Explain-->
 
-<!-- Agenda-->
-<section class="hide-on-small-only">
+<!-- Agenda Desktop-->
+<section class="hide-on-small-only" id="programa">
 	<div class="parallax-overlay">
 	<div id="agenda" class="parallax-container">
 	    <div class="section no-pad-bot">
 	      <div class="container section center">
 	      	<div class="row">
 	      		<div class="center">
-	      			<h2>AGENDA</h2>
+	      			<h2>El Programa</h2>
 	      			<div class="row">
 	      				<img class="img-responsive" src="{{URL::asset('img/agenda/inspirar.png')}}" alt="">
 	      			</div>
@@ -165,19 +164,53 @@
 </section>
 <!-- /Agenda -->
 
+<!-- Agenda Desktop-->
+<section class="hide-on-med-and-up" id="programa">
+	<div class="parallax-overlay">
+	<div id="agenda" class="parallax-container">
+	    <div class="no-pad-bot">
+	      <div class="container center">
+	      	<div class="row center">
+	      			<h2>El Programa</h2>
+	      			<div class="col s12">
+	      					<img class="img-responsive" src="{{URL::asset('img/agenda/inspirar_m.png')}}" alt="" width="300">
+	      			</div>
+	      			<div class="row">
+	      				<img class="img-responsive" src="{{URL::asset('img/agenda/involucrar_m.png')}}" alt="" width="300">
+	      			</div>
+	      			<div class="row">
+	      				<img class="img-responsive" src="{{URL::asset('img/agenda/actuar_m.png')}}" alt="" width="300">
+	      			</div>
+	      	</div>
+	        <div class="row center wow zoomIn" data-wow-delay="0.3s">
+	        	<div class="section"></div>
+	         	<a href="https://podio.com/webforms/17985408/1208462" target="_blank" id="download-button" class="btn-large waves-effect waves-light modal-trigger y-celeste z-depth-3"><strong>Adquiere tu entrada</strong></a>
+	      	</div>
+	      </div>
+	    </div>
+	    <div class="parallax"><img src="{{URL::asset('img/bg/ysf.jpg')}}" alt="Unsplashed background img 1"></div>
+	</div>
+	</div>
+</section>
+<!-- /Agenda -->
+
 <!-- To do: Agenda movil -->
 <!-- /Agenda movil -->
 
 <!--Partners-->
-<section class="container-fluid section">
-    <article class="container">
-        <h4 class="center-align whitespace-short">Ellos confían en #YouthSpeakForum</h4>
-        <div class="your-class row section center">
-            <article class="col s12 m3 offset-m3 l3 offset-l3">
-                <div><img class="responsive-img" src="{{URL::asset('img/logo/aliados/aiesec.png')}}" alt=""></div>
+<section class="container-fluid section" id="ponentes">
+    <article class="container y-azul-text">
+        <h4 class="center-align whitespace-short y-azul-text"><span class="light">Ponentes</span> Magistrales</h4>
+        <div class="row section center">
+            <article class="col s12 m3 offset-m2 l3 offset-l2">
+                <div><img class="responsive-img" src="{{URL::asset('img/ponentes/maria_sacasa.png')}}" alt=""></div>
+                <h4><b>Maria Sacasa</b></h4>
+                <h5 class="light">Coordinadora Residente <br>de ONU Perú</h5>
             </article>
-            <article class="col s12 m3 l3">
-                <div><img class="responsive-img" src="{{URL::asset('img/logo/aliados/onu.png')}}" alt=""></div>
+            <article class="col s12 m3 offset-m2 l3 offset-l2">
+                <div><img class="responsive-img" src="{{URL::asset('img/ponentes/nancy_lange.png')}}" alt=""></div>
+                <h4><b>Nancy Lange</b></h4>
+                <h5 class="light">Primera Dama de la <br>Nación del Perú</h5>
             </article>
         </div>
     </article>
@@ -212,6 +245,37 @@
 </section>
 <!-- /Y4GG -->
 	
+<!--Partners-->
+<section class="container-fluid section" id="aliados">
+    <article class="container y-azul-text">
+        <h4 class="center-align whitespace-short light">Youth Speak Forum es <b>organizado</b> por</h4>
+        <div class="row section center">
+            <article class="col s12 m4 offset-m4 l4 offset-l4">
+                <div><img class="responsive-img" src="{{URL::asset('img/logo/aliados/aiesec.png')}}" alt=""></div>
+            </article>
+        </div>
+        <h4 class="center-align whitespace-short light">Con la <b>colaboración</b> de</h4>
+        <div class="row section center">        
+            <article class="col s12 m4 l4">
+                <div><img class="responsive-img" src="{{URL::asset('img/logo/aliados/plastic_oceans.png')}}" alt=""></div>
+            </article>       
+            <article class="col s12 m4 l4">
+                <div><img class="responsive-img" src="{{URL::asset('img/logo/aliados/onu.png')}}" alt=""></div>
+            </article>       
+            <article class="col s12 m4 l4">
+                <div><img class="responsive-img" src="{{URL::asset('img/logo/aliados/equipu.png')}}" alt=""></div>
+            </article>
+		</div>
+		<div class="row section center">        
+            <article class="col s12 m4 offset-m2 l4 offset-l2">
+                <div><img class="responsive-img" src="{{URL::asset('img/logo/aliados/crea.png')}}" alt=""></div>
+            </article>       
+            <article class="col s12 m4 l4">
+                <div><img class="responsive-img" src="{{URL::asset('img/logo/aliados/wwf.png')}}" alt=""></div>
+            </article>
+		</div>
+    </article>
+</section>
 
 @stop
 
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -17,17 +17,26 @@
 </head>
 <body>
 	<!--MENU-->
-	<nav class="ys-azul" role="navigation">
+	<nav class="ys-azul bg-nav" role="navigation">
 	    <div class="nav-wrapper container">
-	      <a id="logo-container" href="/" class="brand-logo"><img src="{{URL::asset('img/logo/logo.png')}}" width="48px"></a>
+	      <a id="logo-container" href="/" class="brand-logo"><img src="{{URL::asset('img/logo/favicon.png')}}" width="48px"></a>
 	      <ul class="right hide-on-med-and-down">
-	        <li><a href="#agenda" class="white-text">Agenda</a></li>
-	        <li><a href="https://podio.com/webforms/17985408/1208462" target="_blank" class="white-text">Adquiere tu entrada</a></li>
+	        <li><a href="#about" class="white-text">Acerca de YSF</a></li>
+	        <li><a href="#info" class="white-text">Información</a></li>
+	        <li><a href="#metodologia" class="white-text">Metodología</a></li>
+	        <li><a href="#programa" class="white-text">Programa</a></li>
+	        <li><a href="#ponentes" class="white-text">Ponentes</a></li>
+	        <li><a href="https://podio.com/webforms/17985408/1208462" target="_blank" class="white-text"><b>Adquiere tu entrada</b></a></li>
 	      </ul>
 
 	      <ul id="nav-mobile" class="side-nav y-azul">
-	        <li><a href="#agenda" class="white-text">Agenda</a></li>
-	        <li><a href="https://podio.com/webforms/17985408/1208462" target="_blank" class="white-text">Adquiere tu entrada</a></li>
+	        <li><a href="#about" class="white-text">Acerca de YSF</a></li>
+	        <li><a href="#info" class="white-text">Información</a></li>
+	        <li><a href="#metodologia" class="white-text">Metodología</a></li>
+	        <li><a href="#programa" class="white-text">Programa</a></li>
+	        <li><a href="#ponentes" class="white-text">Ponentes</a></li>
+	        <li><a href="#aliados" class="white-text">Aliados</a></li>
+	        <li><a href="https://podio.com/webforms/17985408/1208462" target="_blank" class="white-text"><b>Adquiere tu entrada</b></a></li>
 	      </ul>
 	      <a href="#" data-activates="nav-mobile" class="button-collapse right"><i class="white-text material-icons">menu</i></a>
 	    </div>
@@ -41,8 +50,7 @@
             <div class="row">
               	<div class="col s12 l8">
                 	<h5 class="white-text">#YouthSpeakForum</h5>
-                	<p class="grey-text text-lighten-4">Un lugar donde tus ideas serán escuchadas por los tomadores de decisiones.</p>
-                	<p class="grey-text text-lighten-4">Te inspirarás, te involucrarás y actuarás frente a la realidad del país.</p>
+                	<p class="grey-text text-lighten-4">Un lugar donde la inspiración y la colaboración se convierten en acción.</p>
               	</div>
 
               	<div class="col s12 l1"></div>
